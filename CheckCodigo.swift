3f9bfde68bb96bd2046222eb85ea307f3c15a1f9
Victor M. Ele Ela <eleelavm@gmail.com>
2023-08-06 18:17:07 -0400

    Aug62023

--- CheckCodigo.swift
+++ CheckCodigo.swift
@@ -9,6 +9,7 @@ struct CheckCodigo: View {
     @State private var showAlert2 = false
     @State private var showAlert3 = false
     @State private var showAlert4 = false
+    @State private var showAlertPromotionValid = false
     @State private var input1: String = ""
     @State private var input2: String = ""
     @State private var input3: String = ""
@@ -122,7 +123,7 @@ struct CheckCodigo: View {
         .alert(isPresented: $showAlert) {
             if showAlert1 {
                 return Alert(
-                    title: Text("Codigo validado"),
+                    title: Text("Código validado"),
                     message: Text("Buena suerte."),
                     dismissButton: .default(Text("OK")) {
                         // Reset game data
@@ -138,7 +139,7 @@ struct CheckCodigo: View {
                                 }
                             }
                         }
-
+                        
                         // Navigate to MenuModoCompeticion
                         showAlert1 = false
                         showAlert = false
@@ -158,7 +159,7 @@ struct CheckCodigo: View {
                 )
             } else if showAlert3 {
                 return Alert(
-                    title: Text("Este codigo no existe"),
+                    title: Text("Este código no existe"),
                     message: Text("Intentalo otra vez."),
                     dismissButton: .default(Text("ok")) {
                         clearInputs()
@@ -170,7 +171,7 @@ struct CheckCodigo: View {
             } else if showAlert4 {
                 return Alert(
                     title: Text("Error"),
-                    message: Text("Introduce los 15 digitos del codigo de validacion."),
+                    message: Text("Introduce los 15 digitos del código de validación."),
                     dismissButton: .default(Text("ok")) {
                         clearInputs()
                         showAlert4 = false
@@ -178,80 +179,120 @@ struct CheckCodigo: View {
                         isInput1Active = true
                     }
                 )
+            } else if showAlertPromotionValid {
+                return Alert(title: Text("Código promocional validado. Suerte"))
             } else {
-                return Alert(title: Text(""))
+                return Alert(
+                    title: Text("Unexpected Error"),
+                    message: Text("An unexpected error occurred."),
+                    dismissButton: .default(Text("OK"))
+                )
             }
         }
     }
-    
+
     func checkCodigo() {
-            guard let input = Int("\(input1)\(input2)\(input3)\(input4)") else {
-                showAlert4 = true
-                showAlert = true
-                return
+        guard let input = Int("\(input1)\(input2)\(input3)\(input4)") else {
+            showAlert4 = true
+            showAlert = true
+            return
+        }
+        
+        let gameCodesRef = Database.database().reference().child("gamecodes")
+        gameCodesRef.observeSingleEvent(of: .value) { snapshot in
+            var codeExists = false
+            var codeUsed = false
+            var keyToUpdate: String?
+            var isStaticCode = false
+            
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                if let value = snap.value as? [String: Any] {
+                    if let code = value["code"] as? Int {
+                        if input == code {
+                            codeExists = true
+                            keyToUpdate = snap.key
+                            if let used = value["used"] as? Bool {
+                                codeUsed = used
+                            }
+                            if value["StaticCode"] != nil {
+                                isStaticCode = true
+                            }
+                            break
+                        }
+                    }
+                }
             }
-
-            let gameCodesRef = Database.database().reference().child("gamecodes")
-            gameCodesRef.observeSingleEvent(of: .value) { snapshot in
-                var codeExists = false
-                var codeUsed = false
-                var keyToUpdate: String?
-
-                for child in snapshot.children {
-                    let snap = child as! DataSnapshot
-                    if let value = snap.value as? [String: Any] {
-                        if let code = value["code"] as? Int {
-                            if input == code {
-                                codeExists = true
-                                keyToUpdate = snap.key
-                                if let used = value["used"] as? Bool {
-                                    codeUsed = used
-                                }
-                                break
-                            }
+            
+            if !codeExists {
+                showAlert3 = true
+                showAlert = true
+                clearInputs()
+                isInput1Active = true
+            } else if isStaticCode {
+                showAlertPromotionValid = true
+                showAlert = true
+                clearInputs()
+                isInput1Active = true
+             
+                
+                // Update the game data
+                if let user = Auth.auth().currentUser {
+                    let gameData: [String: Any] = ["currentGameAciertos": 0,
+                                                   "currentGameFallos": 0,
+                                                   "currentGamePuntuacion": 0]
+                    let userRef = Database.database().reference().child("user").child(user.uid)
+                    userRef.updateChildValues(gameData) { (error, reference) in
+                        if let error = error {
+                            print("Failed to update game data. Error: \(error)")
+                            return
                         }
                     }
                 }
 
-                if !codeExists {
-                    showAlert3 = true
-                    showAlert = true
-                    clearInputs()
-                    isInput1Active = true
-                } else if codeUsed {
-                    showAlert2 = true
-                    showAlert = true
-                    clearInputs()
-                    isInput1Active = true
-                } else {
-                    showAlert1 = true
-                    showAlert = true
-                    if let user = Auth.auth().currentUser, let key = keyToUpdate {
-                        let dateFormatter = DateFormatter()
-                        dateFormatter.dateFormat = "dd/MM/yy HH:mm"
-                        let timestamp = dateFormatter.string(from: Date())
-                        let updateValue: [String: Any] = ["used": true,
-                                                          "usedByUserID": user.uid,
-                                                          "usedByfullname": viewModel.fullname,
-                                                          "usedTimestamp": timestamp]
-                        gameCodesRef.child(key).updateChildValues(updateValue) { (error, reference) in
-                            if let error = error {
-                                print("Failed to update value. Error: \(error)")
-                                return
-                            }
+            } else if codeUsed {
+                showAlert2 = true
+                showAlert = true
+                clearInputs()
+                isInput1Active = true
+            } else {
+                showAlert1 = true
+                showAlert = true
+                clearInputs()
+                isInput1Active = true
+            
+                if let user = Auth.auth().currentUser, let key = keyToUpdate {
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "dd/MM/yy HH:mm"
+                    let timestamp = dateFormatter.string(from: Date())
+                    let updateValue: [String: Any] = ["used": true,
+                                                      "usedByUserID": user.uid,
+                                                      "usedByfullname": viewModel.fullname,
+                                                      "usedTimestamp": timestamp]
+                    gameCodesRef.child(key).updateChildValues(updateValue) { (error, reference) in
+                        if let error = error {
+                            print("Failed to update value. Error: \(error)")
+                            return
                         }
                     }
                 }
+                
+                // Navigate to MenuModoCompeticion
+                              showAlertPromotionValid = false
+                              showAlert = false
+                              showSheet = true
             }
         }
-
+    }
+  
+        
         func clearInputs() {
             input1 = ""
             input2 = ""
             input3 = ""
             input4 = ""
         }
-
+        
         struct InputModifier: ViewModifier {
             func body(content: Content) -> some View {
                 content
@@ -263,10 +304,11 @@ struct CheckCodigo: View {
             }
         }
     }
-
-
+    
+    
     struct CheckCodigo_Previews: PreviewProvider {
         static var previews: some View {
             CheckCodigo()
         }
     }
+
